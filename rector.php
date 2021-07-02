8b64cf7022c86765de52a9749912284ef9360aae
Dominik Chrastecky <dominik@chrastecky.cz>
2021-07-02 05:31:34 +0200

    configure rector

--- rector.php
+++ rector.php
@@ -1,18 +1,11 @@
 <?php
 
-use Rector\Core\Configuration\Option;
 use Rector\Set\ValueObject\DowngradeSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-
-    // here we can define, what sets of rules will be applied
-    $parameters->set(Option::SETS, [
-        DowngradeSetList::PHP_80,
-        DowngradeSetList::PHP_74,
-        DowngradeSetList::PHP_73,
-        DowngradeSetList::PHP_72,
-    ]);
+    $containerConfigurator->import(DowngradeSetList::PHP_80);
+    $containerConfigurator->import(DowngradeSetList::PHP_74);
+    $containerConfigurator->import(DowngradeSetList::PHP_73);
+    $containerConfigurator->import(DowngradeSetList::PHP_72);
 };
