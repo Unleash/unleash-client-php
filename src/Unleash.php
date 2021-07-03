f921ce47e6974ee4e157bdbae250386bbff99b8b
Dominik Chrastecky <dominik@chrastecky.cz>
2021-07-04 01:14:28 +0200

    increase version

--- src/Unleash.php
+++ src/Unleash.php
@@ -6,7 +6,7 @@ use Rikudou\Unleash\Configuration\UnleashContext;
 
 interface Unleash
 {
-    public const SDK_VERSION = '0.10.0';
+    public const SDK_VERSION = '0.10.1';
 
     public function isEnabled(string $featureName, UnleashContext $context = null, bool $default = false): bool;
 
