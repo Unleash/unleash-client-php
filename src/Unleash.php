195f8bda0b1c486e874245429b4e398ed42e7160
Dominik Chrastecky <dominik@chrastecky.cz>
2021-07-04 20:09:14 +0200

    increase version number

--- src/Unleash.php
+++ src/Unleash.php
@@ -7,7 +7,7 @@ use Rikudou\Unleash\DTO\Variant;
 
 interface Unleash
 {
-    public const SDK_VERSION = '0.11.0';
+    public const SDK_VERSION = '0.11.1';
 
     public function isEnabled(string $featureName, UnleashContext $context = null, bool $default = false): bool;
 
