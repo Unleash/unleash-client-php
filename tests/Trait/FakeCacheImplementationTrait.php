3af21912240019cde9c049205d0135352c425e72
Dominik Chrastecky <dominik@chrastecky.cz>
2021-07-05 23:22:53 +0200

    make cache mandatory

---
+++ tests/Trait/FakeCacheImplementationTrait.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Rikudou\Tests\Unleash\Trait;
+
+use Psr\SimpleCache\CacheInterface;
+
+trait FakeCacheImplementationTrait
+{
+    private function getCache(): CacheInterface
+    {
+        return new class implements CacheInterface {
+            public function get($key, $default = null)
+            {
+                return $default;
+            }
+
+            public function set($key, $value, $ttl = null): bool
+            {
+                return true;
+            }
+
+            public function delete($key): bool
+            {
+                return true;
+            }
+
+            public function clear(): bool
+            {
+                return true;
+            }
+
+            public function getMultiple($keys, $default = null)
+            {
+                return $default;
+            }
+
+            public function setMultiple($values, $ttl = null): bool
+            {
+                return true;
+            }
+
+            public function deleteMultiple($keys): bool
+            {
+                return true;
+            }
+
+            public function has($key): bool
+            {
+                return false;
+            }
+        };
+    }
+}
