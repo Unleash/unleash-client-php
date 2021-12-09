3b97673bd01075d669004d0057cbad46db3af9ba
Dominik Chrastecky <dominik@chrastecky.cz>
2021-12-09 22:02:02 +0100

    Migrate to PHP 8.1 as default version

--- rector.73.php
+++ rector.73.php
@@ -4,6 +4,7 @@ use Rector\Set\ValueObject\DowngradeSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(DowngradeSetList::PHP_81);
     $containerConfigurator->import(DowngradeSetList::PHP_80);
     $containerConfigurator->import(DowngradeSetList::PHP_74);
 };
