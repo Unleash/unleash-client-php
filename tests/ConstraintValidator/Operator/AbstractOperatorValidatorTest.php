696189d9b5768d158d8ea6ec82ee5f0546f52850
Dominik Chrastecky <dominik@chrastecky.cz>
2022-02-04 21:42:54 +0100

    Add test for invalid type withing multi-value parameters

--- tests/ConstraintValidator/Operator/AbstractOperatorValidatorTest.php
+++ tests/ConstraintValidator/Operator/AbstractOperatorValidatorTest.php
@@ -4,10 +4,12 @@ namespace Unleash\Client\Tests\ConstraintValidator\Operator;
 
 use Unleash\Client\Configuration\UnleashConfiguration;
 use Unleash\Client\ConstraintValidator\Operator\Lists\InListOperatorValidator;
+use Unleash\Client\ConstraintValidator\Operator\String\StringContainsOperatorValidator;
 use Unleash\Client\DefaultUnleash;
 use Unleash\Client\DTO\Feature;
 use Unleash\Client\DTO\Variant;
 use Unleash\Client\Enum\ConstraintOperator;
+use Unleash\Client\Exception\OperatorValidatorException;
 use Unleash\Client\Metrics\MetricsHandler;
 use Unleash\Client\Stickiness\MurmurHashCalculator;
 use Unleash\Client\Strategy\DefaultStrategyHandler;
@@ -19,11 +21,25 @@ final class AbstractOperatorValidatorTest extends AbstractHttpClientTest
 {
     use FakeCacheImplementationTrait;
 
-    public function testInvoke()
+    public function testInvokeNull()
     {
         $instance = new InListOperatorValidator();
-        self::assertFalse($instance('test', 'test'));
-        self::assertFalse($instance('test', null));
+        $this->expectException(OperatorValidatorException::class);
+        $instance('test', null);
+    }
+
+    public function testInvokeUnacceptableType()
+    {
+        $instance = new InListOperatorValidator();
+        $this->expectException(OperatorValidatorException::class);
+        $instance('test', 'test');
+    }
+
+    public function testInvokeMultipleValuesUnacceptableType()
+    {
+        $instance = new StringContainsOperatorValidator();
+        $this->expectException(OperatorValidatorException::class);
+        $instance('test', ['some-value', [], 'test']);
     }
 
     public function testMissingValues()
@@ -65,7 +81,30 @@ final class AbstractOperatorValidatorTest extends AbstractHttpClientTest
                 ],
             ],
         ]);
+        $this->pushResponse([
+            'features' => [
+                [
+                    'name' => 'test',
+                    'description' => '',
+                    'enabled' => true,
+                    'strategies' => [
+                        [
+                            'name' => 'default',
+                            'constraints' => [
+                                [
+                                    'contextName' => 'email',
+                                    'operator' => ConstraintOperator::STRING_STARTS_WITH,
+                                    'inverted' => true,
+                                    // missing 'value' and 'values' and 'inverted' set to true
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ]);
 
+        self::assertFalse($unleash->isEnabled('test'));
         self::assertFalse($unleash->isEnabled('test'));
     }
 }
