79bed1f55dfa7c026d77362a34c56f34ef99b820
Dominik Chrastecky <dominik@chrastecky.cz>
2022-11-29 13:41:08 +0100

    Chore: Update rector and rules

--- rector.81.php
+++ rector.81.php
@@ -5,6 +5,6 @@ use Rector\Set\ValueObject\DowngradeLevelSetList;
 
 return static function (RectorConfig $config): void {
     $config->sets([
-        DowngradeLevelSetList::DOWN_TO_PHP_72,
+        DowngradeLevelSetList::DOWN_TO_PHP_81,
     ]);
 };
