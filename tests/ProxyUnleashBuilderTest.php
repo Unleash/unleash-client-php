2c2d86b6ca0af847992030d5a0ada13f1d7391be
sighphyre <liquidwicked64@gmail.com>
2023-07-18 10:29:21 +0200

    chore: rework failing test

--- tests/ProxyUnleashBuilderTest.php
+++ tests/ProxyUnleashBuilderTest.php
@@ -2,6 +2,7 @@
 
 namespace Unleash\Client\Tests;
 
+use Cache\Adapter\Filesystem\FilesystemCachePool;
 use Http\Discovery\Psr18ClientDiscovery;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Client\ClientInterface;
@@ -10,10 +11,7 @@ use ReflectionObject;
 use Symfony\Component\Cache\Psr16Cache;
 use Unleash\Client\Configuration\UnleashConfiguration;
 use Unleash\Client\DefaultProxyUnleash;
-use Unleash\Client\DTO\Feature;
-use Unleash\Client\DTO\Variant;
 use Unleash\Client\Exception\InvalidValueException;
-use Unleash\Client\Metrics\MetricsHandler;
 use Unleash\Client\ProxyUnleashBuilder;
 use Unleash\Client\Tests\Traits\RealCacheImplementationTrait;
 
@@ -62,64 +60,6 @@ final class ProxyUnleashBuilderTest extends TestCase
         self::assertNotSame($this->instance, $this->instance->withRequestFactory($this->newRequestFactory()));
     }
 
-    public function testWithHeader()
-    {
-        self::assertNotSame($this->instance, $this->instance->withHeader('Authorization', 'test'));
-
-        $instance = $this->instance
-            ->withHeader('Authorization', 'test')
-            ->withHeader('Some-Header', 'test');
-        $reflection = new ReflectionObject($instance);
-        $headersProperty = $reflection->getProperty('headers');
-        $headersProperty->setAccessible(true);
-        $headers = $headersProperty->getValue($instance);
-        self::assertCount(2, $headers);
-
-        $instance = $instance
-            ->withHeader('Authorization', 'test2');
-        $headers = $headersProperty->getValue($instance);
-        self::assertCount(2, $headers);
-        self::assertArrayHasKey('Authorization', $headers);
-        self::assertEquals('test2', $headers['Authorization']);
-
-        $instance = $instance
-            ->withHeaders([
-                'Some-Header-2' => 'value',
-                'Some-Header-3' => 'value',
-            ]);
-        $headers = $headersProperty->getValue($instance);
-        self::assertCount(2, $headers);
-        self::assertArrayHasKey('Some-Header-2', $headers);
-        self::assertArrayHasKey('Some-Header-3', $headers);
-    }
-
-    public function testWithMetricsInterval()
-    {
-        self::assertNotSame($this->instance, $this->instance->withMetricsInterval(5000));
-    }
-
-    public function testWithMetricsEnabled()
-    {
-        self::assertNotSame($this->instance, $this->instance->withMetricsEnabled(false));
-    }
-
-    public function testWithMetricsHandler()
-    {
-        $metricsHandler = new class implements MetricsHandler {
-            public function handleMetrics(Feature $feature, bool $successful, Variant $variant = null): void
-            {
-            }
-        };
-        $instance = $this->instance
-            ->withMetricsHandler($metricsHandler)
-        ;
-        self::assertNotSame($this->instance, $instance);
-        self::assertSame(
-            $metricsHandler,
-            $this->getProperty($instance->withAppUrl('http://test.com')->withInstanceId('test')->withAppName('test-app')->build(), 'metricsHandler')
-        );
-    }
-
     public function testWithoutDefaultCache()
     {
         $instance = $this->instance
@@ -138,19 +78,35 @@ final class ProxyUnleashBuilderTest extends TestCase
 
         $defaultImplementationsProperty = (new ReflectionObject($locator))->getProperty('defaultImplementations');
         $defaultImplementationsProperty->setAccessible(true);
-
         $defaultImplementations = $defaultImplementationsProperty->getValue($locator);
 
         if (class_exists(FilesystemCachePool::class)) {
-            $this->assertDefaultCacheImplementation($instance, $cacheProperty, FilesystemCachePool::class, $locator, $defaultImplementationsProperty, $defaultImplementations);
+            $cache = $cacheProperty->getValue($unleash);
+            self::assertInstanceOf(FilesystemCachePool::class, $cache);
+            $defaultImplementations['cache'][FilesystemCachePool::class . 2] = [];
+            unset($defaultImplementations['cache'][FilesystemCachePool::class]);
+            $defaultImplementationsProperty->setValue($locator, $defaultImplementations);
         }
 
-        if (class_exists(Psr16Cache::class)) {
-            $this->assertDefaultCacheImplementation($instance, $cacheProperty, Psr16Cache::class, $locator, $defaultImplementationsProperty, $defaultImplementations);
-        }
+        $unleash = $instance->build();
+        $cache = $cacheProperty->getValue($unleash);
 
-        $this->expectException(InvalidValueException::class);
-        $instance->build();
+        self::assertInstanceOf(Psr16Cache::class, $cache);
+
+        $defaultImplementations['cache'][Psr16Cache::class . 2] = [];
+        unset($defaultImplementations['cache'][Psr16Cache::class], $defaultImplementations['cache'][FilesystemCachePool::class]);
+
+        $defaultImplementationsProperty->setValue($locator, $defaultImplementations);
+
+        $cache = $cacheProperty->getValue($unleash);
+        unset($defaultImplementations['cache'][Psr16Cache::class]);
+        $defaultImplementationsProperty->setValue($locator, $defaultImplementations);
+
+        try {
+            $instance->build();
+            $this->fail('No default cache implementation is available, expected exception');
+        } catch (InvalidValueException $e) {
+        }
     }
 
     public function testWithoutDefaultHttpClient()
@@ -302,16 +258,5 @@ final class ProxyUnleashBuilderTest extends TestCase
         $property->setAccessible(true);
 
         return $property->getValue($object);
-    }
-
-    private function assertDefaultCacheImplementation($instance, $cacheProperty, $className, $locator, $defaultImplementationsProperty, $defaultImplementations)
-    {
-        $unleash = $instance->build();
-        $cache = $cacheProperty->getValue($unleash);
-
-        self::assertInstanceOf($className, $cache);
-
-        unset($defaultImplementations['cache'][$className]);
-        $defaultImplementationsProperty->setValue($locator, $defaultImplementations);
     }
 }
