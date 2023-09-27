0f7d34eb638d377cfee9d432fe3e8eaeff6ce84c
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-09-27 15:15:18 +0200

    update spec version

--- src/Repository/DefaultUnleashRepository.php
+++ src/Repository/DefaultUnleashRepository.php
@@ -98,7 +98,7 @@ final class DefaultUnleashRepository implements UnleashRepository
                     ->createRequest('GET', $this->configuration->getUrl() . 'client/features')
                     ->withHeader('UNLEASH-APPNAME', $this->configuration->getAppName())
                     ->withHeader('UNLEASH-INSTANCEID', $this->configuration->getInstanceId())
-                    ->withHeader('Unleash-Client-Spec', '4.3.2')
+                    ->withHeader('Unleash-Client-Spec', '4.5.0')
                 ;
 
                 foreach ($this->configuration->getHeaders() as $name => $value) {
