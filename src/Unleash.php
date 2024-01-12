8d9af1d6800d002b68ac28ed90141ed30488f162
Dominik Chrastecky <dominik@chrastecky.cz>
2024-01-12 12:37:36 +0100

    Increase version before release

--- src/Unleash.php
+++ src/Unleash.php
@@ -7,7 +7,7 @@ use Unleash\Client\DTO\Variant;
 
 interface Unleash
 {
-    public const SDK_VERSION = '1.13.0';
+    public const SDK_VERSION = '1.13.1';
 
     public function isEnabled(string $featureName, ?Context $context = null, bool $default = false): bool;
 
