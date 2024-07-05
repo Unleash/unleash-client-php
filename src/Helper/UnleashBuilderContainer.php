402929a3e4e201e40afa470e99886871ba40ebe4
Michael Schmetter <michael.schmetter@aldi-sued.com>
2024-07-05 11:46:18 +0200

    Feat: Added possibility to create the Repository in the Builder - refactored the UnleashBuilder.php to enable it to also build only the repository

--- src/Helper/UnleashBuilderContainer.php
+++ src/Helper/UnleashBuilderContainer.php
@@ -5,7 +5,12 @@ namespace Unleash\Client\Helper;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\SimpleCache\CacheInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Unleash\Client\Bootstrap\BootstrapHandler;
+use Unleash\Client\Bootstrap\BootstrapProvider;
 use Unleash\Client\Configuration\UnleashConfiguration;
+use Unleash\Client\ContextProvider\UnleashContextProvider;
+use Unleash\Client\Metrics\MetricsBucketSerializer;
 use Unleash\Client\Metrics\MetricsSender;
 use Unleash\Client\Stickiness\StickinessCalculator;
 
@@ -19,9 +24,15 @@ final readonly class UnleashBuilderContainer
         private CacheInterface $staleCache,
         private ClientInterface $httpClient,
         private ?MetricsSender $metricsSender,
+        private CacheInterface $metricsCache,
         private RequestFactoryInterface $requestFactory,
         private StickinessCalculator $stickinessCalculator,
         private ?UnleashConfiguration $configuration,
+        private UnleashContextProvider $contextProvider,
+        private BootstrapHandler $bootstrapHandler,
+        private BootstrapProvider $bootstrapProvider,
+        private EventDispatcherInterface $eventDispatcher,
+        private MetricsBucketSerializer $metricsBucketSerializer,
     ) {
     }
 
@@ -58,5 +69,35 @@ final readonly class UnleashBuilderContainer
     public function getConfiguration(): ?UnleashConfiguration
     {
         return $this->configuration;
+    }
+
+    public function getMetricsCache(): CacheInterface
+    {
+        return $this->metricsCache;
+    }
+
+    public function getContextProvider(): UnleashContextProvider
+    {
+        return $this->contextProvider;
+    }
+
+    public function getBootstrapHandler(): BootstrapHandler
+    {
+        return $this->bootstrapHandler;
+    }
+
+    public function getBootstrapProvider(): BootstrapProvider
+    {
+        return $this->bootstrapProvider;
+    }
+
+    public function getEventDispatcher(): EventDispatcherInterface
+    {
+        return $this->eventDispatcher;
+    }
+
+    public function getMetricsBucketSerializer(): MetricsBucketSerializer
+    {
+        return $this->metricsBucketSerializer;
     }
 }
