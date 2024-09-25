cac078ec4cd7e6426428e64b226b2684458b3451
Gastón Fournier <gaston@getunleash.io>
2024-09-25 18:21:30 +0200

    code format

--- src/DTO/DefaultProxyFeature.php
+++ src/DTO/DefaultProxyFeature.php
@@ -38,7 +38,8 @@ final readonly class DefaultProxyFeature implements ProxyFeature, JsonSerializab
         // tries to new this up manually and isn't interesting to tests
 
         // @codeCoverageIgnoreStart
-        assert(isset($response['name'], $response['enabled'], $response['variant']) 
+        assert(
+            isset($response['name'], $response['enabled'], $response['variant'])
             && (isset($response['impressionData']) || isset($response['impression_data']))
         );
         // @codeCoverageIgnoreEnd
