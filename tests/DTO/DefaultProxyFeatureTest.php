07b6c4f43985ea7fd96c758cbde29322c334a8af
Gastón Fournier <gaston@getunleash.io>
2024-09-26 16:28:00 +0200

    test: verify backward compatibility

--- tests/DTO/DefaultProxyFeatureTest.php
+++ tests/DTO/DefaultProxyFeatureTest.php
@@ -22,9 +22,30 @@ final class DefaultProxyFeatureTest extends TestCase
         self::assertEquals('someVariant', $instance->getVariant()->getName());
     }
 
+    public function testGetPropertiesImpressionDataSnakeCaseBackwardsCompatibility()
+    {
+        $instance = new DefaultProxyFeature(['name' => 'test', 'enabled' => true, 'impression_data' => true, 'variant' => ['name' => 'someVariant', 'enabled' => true, 'payload' => ['type' => 'string', 'value' => 'test']]]);
+        self::assertIsString($instance->getName());
+        self::assertIsBool($instance->isEnabled());
+        self::assertIsBool($instance->hasImpressionData());
+        self::assertInstanceOf(DefaultVariant::class, $instance->getVariant());
+
+        self::assertEquals('test', $instance->getName());
+        self::assertTrue($instance->isEnabled());
+        self::assertTrue($instance->hasImpressionData());
+        self::assertEquals('someVariant', $instance->getVariant()->getName());
+    }
+
     public function testToJson()
     {
         $instance = new DefaultProxyFeature(['name' => 'test', 'enabled' => true, 'impressionData' => true, 'variant' => ['name' => 'someVariant', 'enabled' => true, 'payload' => ['type' => 'string', 'value' => 'test']]]);
+        $json = json_encode($instance);
+        self::assertEquals('{"name":"test","enabled":true,"variant":{"name":"someVariant","enabled":true,"payload":{"type":"string","value":"test"}},"impression_data":true,"impressionData":true}', $json);
+    }
+
+    public function testToJsonFromSnakeCaseImpressionDataForBackwardsCompatibility()
+    {
+        $instance = new DefaultProxyFeature(['name' => 'test', 'enabled' => true, 'impression_data' => true, 'variant' => ['name' => 'someVariant', 'enabled' => true, 'payload' => ['type' => 'string', 'value' => 'test']]]);
         $json = json_encode($instance);
         self::assertEquals('{"name":"test","enabled":true,"variant":{"name":"someVariant","enabled":true,"payload":{"type":"string","value":"test"}},"impression_data":true,"impressionData":true}', $json);
     }
